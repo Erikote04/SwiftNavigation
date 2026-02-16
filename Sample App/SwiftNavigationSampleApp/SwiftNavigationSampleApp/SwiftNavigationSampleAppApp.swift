7737a9836327090df0965f496adb40fb18e7d986
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2026-02-16 15:53:14 +0100

    feat(app): init project

---
+++ Sample App/SwiftNavigationSampleApp/SwiftNavigationSampleApp/SwiftNavigationSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNavigationSampleAppApp.swift
+//  SwiftNavigationSampleApp
+//
+//  Created by Erik Sebastian de Erice Jerez on 16/2/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNavigationSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
